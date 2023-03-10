b7cb21192c33b76bad9945f952dadcf4a5d6c63d
Angelina <angelinaivanova16@gmail.com>
2023-03-10 17:23:52 +0300

    add changing count of products with hook usestate

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -2,8 +2,17 @@ import './products.css'
 import { products } from '../../products';
 import Card from '../../components/elements/card';
 import { Link } from "react-router-dom";
+import React, { useState } from 'react';
 
 function Products () {
+  const [count, setCount] = useState(0);
+  const [countPrice, setCountPrice] = useState(0);
+  let p;
+  const getPrice = () => {
+    setCount(count + 1);
+    setCountPrice(countPrice + p)
+  }
+
   return (
     <main className="main">
       <div className='container'>
@@ -11,8 +20,8 @@ function Products () {
           <h1 className='header-title'>наша продукция</h1>
           <div className="header-basket">
             <div className='header-basket-text'>
-              <p>3 товара</p>
-              <p>на сумму 3 500 ₽</p>
+              <p>{count} товар(а)</p>
+              <p>на сумму {countPrice} ₽</p>
             </div>
             <Link to={'/basket'}>
               <div className='header-basket-image'></div>
@@ -23,13 +32,15 @@ function Products () {
         <div className='products-list'>
           {products.map(item => {
             return (
-              <Card 
+              // <Card click={() => setCount(count + 1)}
+              <Card click={getPrice}
               key={item.id}
               img={item.url}
               title={item.title}
               description={item.description}
               price={item.price}
-              weight={item.weight} />
+              weight={item.weight
+              } />
             )
           })}
         </div>
