d6fb874a36362a813b70e0222052e64430a5aaaa
Angelina <angelinaivanova16@gmail.com>
2023-03-20 10:59:41 +0300

    add layout and styles to page description

--- src/pages/description/index.jsx
+++ src/pages/description/index.jsx
@@ -1,8 +1,26 @@
 import './description.css'
+import { Link } from 'react-router-dom';
+import Button from '../../components/ui/button';
 
 function Description () {
   return (
-    <div>Описание товара</div>
+    <div className='description-container'>
+      <div className='description-wrapper'>
+        <header className='description-header'>
+          <Link to={'/products'}><div className='basket-arrow'></div></Link>
+          <div className='description-rightside'>
+            <div className='header-description-text'>
+              <p>0 товар(а)</p>
+              <p>на сумму 0 ₽</p>
+            </div>
+            <Link to={'/basket'}>
+              <div className='header-basket-image'></div>
+            </Link>
+            <Button btnName='Выйти'/>
+          </div>
+        </header>
+      </div>
+    </div>
   )
 }
 
