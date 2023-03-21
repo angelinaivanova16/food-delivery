5f01f19634bdd79260f35ba9d04e2f58366bbf06
Angelina <angelinaivanova16@gmail.com>
2023-03-21 23:42:27 +0300

    implement adding to basket at the page description

--- src/components/elements/cardDescription/index.jsx
+++ src/components/elements/cardDescription/index.jsx
@@ -1,7 +1,25 @@
 import Button from '../../ui/button';
 import './cardDescription.css'
+import { useDispatch } from 'react-redux';
+import { addProductsBasket } from '../../../store/reducers/products';
+import uuid from 'react-uuid';
 
-function CardDescription ({img, title, full, price, weight}) {
+function CardDescription ({id, img, title, full, price, weight}) {
+  const dispatch = useDispatch();
+
+  const addProduct = () => {
+    const item = {
+      id: id,
+      idx: uuid(),
+      title: title,
+      img: img,
+      price: price,
+      full: full
+    }
+
+    dispatch(addProductsBasket(item));
+  }
+
   return(
     <div className='card-description'>
       <img className='card-description-image' src={img} alt="foodImage" />
@@ -10,7 +28,7 @@ function CardDescription ({img, title, full, price, weight}) {
         <p className='card-description-text'>{full}</p>
         <div className='card-description-count'>
           <p className='card-description-price'>{price} ₽ <span className='card-description-weight'>/ {weight} г.</span></p>
-          <Button btnName='В корзину'/>
+          <Button btnName='В корзину' click={addProduct}/>
         </div>
       </div>
     </div>
