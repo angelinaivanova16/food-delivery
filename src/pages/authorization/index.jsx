ac9444fa8fca342ceb9617bc9fefbc0417b4d98e
Angelina <angelinaivanova16@gmail.com>
2023-03-24 01:50:27 +0300

    implement forms registration and authorization

--- src/pages/authorization/index.jsx
+++ src/pages/authorization/index.jsx
@@ -1,5 +1,50 @@
 import './authorization.css'
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+function checkFormAuthorization(e) {
+  const fieldLogin2 = document.getElementById('login');
+  const fieldPassword2 = document.getElementById('password')
+  const notification1_2 = document.getElementById('notification1');
+  const notification2_2 = document.getElementById('notification2');
+  const notification3 = document.getElementById('notification3');
+  // const btn = document.querySelector('.form-registration__btn');
+  let hasEmpty = false; // Поля заполнены - это у нас будет false
+
+  if(!fieldLogin2.value) {   // если это пустое поле,
+    notification1_2.textContent = 'Поле не должно быть пустым';
+    // btn.classList.add('higher');
+    hasEmpty = true;   // тогда true - значит поле пустое
+  } else {
+    notification1_2.textContent = '';
+  }
+
+  if(!fieldPassword2.value) {
+    notification2_2.textContent = 'Поле не должно быть пустым';
+    hasEmpty = true;
+  } else {
+    notification2_2.textContent = '';
+  }
+
+  if(!hasEmpty) {   // Если что-то не заполнено или неверно, то НЕ отправляем форму
+    e.preventDefault();
+    let obj2 = {
+      login: fieldLogin2.value,
+      password: fieldPassword2.value,
+    };
+
+    let currentArr2 = JSON.parse(localStorage.getItem("users")); // Записываем в currentArr2 наш массив с данными из localStorage
+    // Объект с объектом не сравниваются обычным способом, так как это ссылочный тип данных, поэтому сконвертируем в строки
+    if (JSON.stringify(currentArr2).includes(JSON.stringify(obj2))) {
+      window.location.href = '/products';
+    } else {
+      notification3.textContent = 'Логин или пароль неверен';
+    }   
+  }
+}
+
+
+
 
 function Authorization () {
   return (
@@ -39,7 +84,7 @@ function Authorization () {
           </div>
           <p className="form-authorization__notification" id="notification3"></p>
 
-          <button className="form-authorization__btn" type="submit">
+          <button className="form-authorization__btn" type="submit" onClick={checkFormAuthorization}>
           Войти
           </button>
         </div>
--- src/pages/registration/index.jsx
+++ src/pages/registration/index.jsx
@@ -1,6 +1,60 @@
 import './registration.css'
 import { Link } from 'react-router-dom';
 
+function checkFormRegistration(e) {
+  const fieldLogin = document.getElementById('login');
+  const fieldPassword = document.getElementById('password')
+  const notification1 = document.getElementById('notification1');
+  const notification2 = document.getElementById('notification2');
+  // const btn = document.querySelector('.form-registration__btn');
+  let hasEmpty = false; // Поля заполнены - это у нас будет false
+
+  if(!fieldLogin.value) {   // если это пустое поле,
+    notification1.textContent = 'Поле не должно быть пустым';
+    // btn.classList.add('higher');
+    hasEmpty = true;   // тогда true - значит поле пустое
+  } else if(fieldLogin.value.length < 4) {
+    notification1.textContent = 'Логин должен содержать не менее 4-х символов';
+    // btn.classList.add('higher');
+    hasEmpty = true;
+  } else {
+    notification1.textContent = '';
+  }
+
+  if(!fieldPassword.value) {
+    notification2.textContent = 'Поле не должно быть пустым';
+    hasEmpty = true;
+  } else if(fieldPassword.value.length < 4) {
+    notification2.textContent = 'Пароль должен содержать не менее 4-х символов';
+    hasEmpty = true;
+  } else {
+    notification2.textContent = '';
+  }
+
+  if(hasEmpty) {   // Если что-то не заполнено или неверно, то НЕ отправляем форму
+    e.preventDefault()   
+  }
+
+  if(!hasEmpty) {     // Если все поля заполнены (hasEmpty = false), то сохраняем массив объектов с данными.
+    let obj = {
+      login: fieldLogin.value,
+      password: fieldPassword.value,
+    };
+
+    let currentArr = JSON.parse(localStorage.getItem("users")); // По ключу users будем смотреть, есть ли уже что-то в localStorage
+    if (currentArr == null) {
+      let arr = [];
+      arr.push(obj);
+      localStorage.setItem('users', JSON.stringify(arr));
+    } else {
+      console.log(currentArr)
+      currentArr.push(obj)
+      localStorage.setItem('users', JSON.stringify(currentArr));
+    }
+  }
+}
+
+
 function Registration () {
   return (
     <div className='registration-container'>
@@ -8,7 +62,6 @@ function Registration () {
         <Link to={'/authorization'}>
           <p className="form-registration__subtitle">Авторизоваться</p>
         </Link>
-        {/* <p className="form-registration__subtitle">Авторизоваться</p> */}
         <div className="form-registration__wrapper">
           <h1 className="form-registration__title">pегистрация</h1>
           <input
@@ -40,7 +93,7 @@ function Registration () {
           </div>
           <p className="form-registration__notification" id="notification3"></p>
 
-          <button className="form-registration__btn" type="submit">
+          <button className="form-registration__btn" type="submit" onClick={checkFormRegistration}>
             Зарегистрироваться
           </button>
         </div>
